0b90d5cab58a04264828db3a0d9924aa7fe6953f
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2021-10-18 00:34:08 +0100

    tests

--- bank/src/main/java/enit/bank/controller/BankController.java
+++ bank/src/main/java/enit/bank/controller/BankController.java
@@ -6,7 +6,6 @@ import enit.bank.domain.resource.ClientTransaction;
 import enit.bank.service.BankService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -17,36 +16,38 @@ import javax.ws.rs.core.Response;
 
 @Path("bank")
 @ApplicationScoped
-@Produces("application/json")
-@Consumes("application/json")
 @Slf4j
 public class BankController {
-    private static Logger logger;
-
     @Inject
     BankService bankService;
 
     @POST
-    @Path("/transaction/make")
+    @Path("/transaction/make/{accountno}")
     @Transactional
-    public Response MakeTransaction(AccountTransaction accountTransaction) {
-        logger.info(String.valueOf(accountTransaction));
+    @Consumes("application/json")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response MakeTransaction(@PathParam Long code, AccountTransaction accountTransaction) {
+        BankAccount ba = bankService.getAccount(code)
         bankService.MakeTransaction(accountTransaction);
+        ba.getAccountTransactionSet().add(accountTransaction);
+        bankService.CreateAccount(ba);
         return Response.ok(accountTransaction).status(201).build();
     }
 
     @POST
     @Path("/account/make")
     @Transactional
-    public Response MakeTransaction(BankAccount bankAccount) {
-        logger.info(String.valueOf(bankAccount));
+    @Consumes("application/json")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String MakeAccount(BankAccount bankAccount) {
         bankService.CreateAccount(bankAccount);
-        return Response.ok(bankAccount).status(201).build();
+        return bankAccount.toString();
     }
     @GET
-    @Path("/hello")
+    @Path("/testresource")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String test(ClientTransaction c){
+        return c.toString();
     }
+
 }
