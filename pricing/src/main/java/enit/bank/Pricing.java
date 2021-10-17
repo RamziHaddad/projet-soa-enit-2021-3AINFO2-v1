7d20dcde5910d1bf127256d32e853e080eb0430b
marwa-faidi123 <62595529+marwa-faidi123@users.noreply.github.com>
2021-10-17 22:04:29 +0000

    Pricing method

--- pricing/src/main/java/enit/bank/Pricing.java
+++ pricing/src/main/java/enit/bank/Pricing.java
@@ -1,10 +1,10 @@
 package enit.bank;
-
 import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
 
 @Path("/api/price")
 public class Pricing {
@@ -14,73 +14,80 @@ public class Pricing {
 
     @POST
     @Path("/price/set/new")
-    protected void affecterPrix(float price) {
-
+    protected void affecterPrix(float price){
+        this.price=price;
     }
 
     @POST
     @Path("/price/set/maj")
-    protected void majPrix(float prix) {
+    protected void majPrix(float newPrice){
+        this.price= newPrice;
 
     }
 
     @GET
     @Path("/price/product")
-    public float getPrixProduit(int id) {
-        return 0;
+    public String getPrixProduit (int idProduit){
+        if(this.id==idProduit){
+            return String.valueOf(this.price);
+        }
+        else {
+            return ("le produit n'existe pas") ;
+        }
     }
 
     @POST
     @Path("/promo/set/product")
-    protected void ajouterPromoProduit(float promo) {
-
+    protected void ajouterPromoProduit(int idProduit, float newPromo){
+        this.promo= newPromo;
     }
 
     @POST
     @Path("/promo/maj/product")
-    protected void majPromoProduit(float promo) {
+    protected void majPromoProduit(float promoMAJ){
+        this.promo=promoMAJ;
 
     }
 
     @PUT
     @Path("/promo/activate/product")
-    protected void activerPromoProduit() {
+    protected void activerPromoProduit(){
 
     }
 
     @DELETE
     @Path("/promo/desactivate/product")
-    protected void desactiverPromoProduit() {
+    protected void desactiverPromoProduit(){
 
     }
 
     @POST
     @Path("/promo/set/Categorie")
-    protected void ajouterPromoCategorie(float promo) {
+    protected void ajouterPromoCategorie(float promo){
 
     }
 
     @POST
     @Path("/promo/maj/Categorie")
-    protected void majPromoCategorie(float promo) {
+    protected void majPromoCategorie(float promo){
 
     }
 
     @PUT
     @Path("/promo/activate/Categorie")
-    protected void activerPromoCategorie() {
+    protected void activerPromoCategorie(){
 
     }
 
     @DELETE
     @Path("/promo/desactivate/Categorie")
-    protected void desactiverPromoCategorie() {
+    protected void desactiverPromoCategorie(){
 
     }
 
     @GET
     @Path("/price/total")
-    public float getPrixTotal(int id, int quantity) {
+    public float getPrixTotal(int id, int quantity){
         return 0;
     }
 }
