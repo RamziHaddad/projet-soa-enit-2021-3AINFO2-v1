04439a44fa921c1ce8e651e501f7fe4af56faefc
Aziz Bhiri <aziz.bhiri@etudiant-enit.utm.tn>
2021-10-19 19:47:15 +0100

    modified the structure

--- inventory/src/main/java/enit/bank/Service/ProductService.java
+++ inventory/src/main/java/enit/bank/Service/ProductService.java
@@ -1,47 +1,6 @@
-import java.util.*;
-
-class Product 
-{
-  	private int m_prodID;
-	private String m_prodName;
-	
-	Product() {}
-	Product(int newID, String newName)	// Overload the constructor
-	{
-		m_prodID = newID;
-		m_prodName = newName;
-	}
-
-	// Methods to get and set the values
-	public int GetID()  {
-		return m_prodID;
-	}
-	public String GetName() {
-		return m_prodName;
-	}
-	public void SetID(int newID) {
-		m_prodID = newID;
-	}
-	public void SetName(String newName) {
-		m_prodName = newName;
-	}
-
-	// Method to print out our data
-	public void PrintValues() {
-		System.out.println(m_prodID + " " + m_prodName);
-	}
-
-	// New method to see if two products are equal. 
-	public boolean equals(Object otherProduct)
-	{
-		Product p = (Product) otherProduct;
-		return (this.m_prodID == p.m_prodID);
-	}
-}
-
-class Products
-{
-	private Vector m_vProds;
+import enit.bank.*;
+public class ProductService {
+    private Vector m_vProds;
 
 	public Products()
 	{
@@ -98,7 +57,5 @@ class Products
 			p = (Product) m_vProds.elementAt(i);
 			p.PrintValues();
 		}
-	}
+	} 
 }
-
-//merge test
