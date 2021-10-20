24051eb6d6ad1d533ec9a40b87729fd622e39c0e
Rahma Boughale <72996581+rahma2020817@users.noreply.github.com>
2021-10-21 00:03:48 +0100

    update

---
+++ pricing/src/main/java/enit/bank/Domain/Cart.java
@@ -0,0 +1,20 @@
+package enit.bank.Domain;
+
+public class Cart {
+    private Long idCart;
+
+    /**
+     * @return Long return the idCart
+     */
+    public Long getIdCart() {
+        return idCart;
+    }
+
+    /**
+     * @param idCart the idCart to set
+     */
+    public void setIdCart(Long idCart) {
+        this.idCart = idCart;
+    }
+
+}
--- pricing/src/main/java/enit/bank/Pricing.java
+++ pricing/src/main/java/enit/bank/Pricing.java
@@ -1,70 +1,73 @@
 package enit.bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import antlr.collections.List;
-
+import enit.bank.Domain.Cart;
 
 @Path("/api/price")
 public class Pricing {
     int id;
     float price;
     float promo;
+    public List<Cart> CartList = new ArrayList<Cart>();
 
     @POST
     @Path("/price/set/new")
-    protected void affecterPrix(float price){
-        this.price=price;
+    protected void affecterPrix(float price) {
+        this.price = price;
     }
 
     @POST
     @Path("/price/set/maj")
-    protected void majPrix(float newPrice){
-        this.price= newPrice;
+    protected void majPrix(float newPrice) {
+        this.price = newPrice;
 
     }
 
     @GET
     @Path("/price/product")
-    public String getPrixProduit (int idProduit){
-        if(this.id==idProduit){
+    public String getPrixProduit(int idProduit) {
+        if (this.id == idProduit) {
             return String.valueOf(this.price);
-        }
-        else {
-            return ("le produit n'existe pas") ;
+        } else {
+            return ("le produit n'existe pas");
         }
     }
 
     @POST
     @Path("/promo/set/product")
-    protected void ajouterPromoProduit(int idProduit, float newPromo){
-        this.promo= newPromo;
+    protected void ajouterPromoProduit(int idProduit, float newPromo) {
+        this.promo = newPromo;
     }
 
     @POST
     @Path("/promo/maj/product")
-    protected void majPromoProduit(float promoMAJ){
-        this.promo=promoMAJ;
+    protected void majPromoProduit(float promoMAJ) {
+        this.promo = promoMAJ;
 
     }
 
     @PUT
     @Path("/promo/activate/product")
-    protected void activerPromoProduit(float promo,int idProduit){
-        if(this.id==idProduit){
-            float prixPromo= this.price- (this.price*promo);
+    protected void activerPromoProduit(float promo, int idProduit) {
+        if (this.id == idProduit) {
+            float prixPromo = this.price - (this.price * promo);
             majPrix(prixPromo);
         }
     }
 
     @DELETE
     @Path("/promo/desactivate/product")
-    protected void desactiverPromoProduit(float promo,int idProduit){
-        if(this.id==idProduit){
-            float oldPrice= this.price+(this.price*promo);
+    protected void desactiverPromoProduit(float promo, int idProduit) {
+        if (this.id == idProduit) {
+            float oldPrice = this.price + (this.price * promo);
             majPrix(oldPrice);
         }
 
@@ -72,30 +75,87 @@ public class Pricing {
 
     @POST
     @Path("/promo/set/Categorie")
-    protected void ajouterPromoCategorie(){
+    protected void ajouterPromoCategorie() {
     }
 
     @POST
     @Path("/promo/maj/Categorie")
-    protected void majPromoCategorie(float promo){
+    protected void majPromoCategorie(float promo) {
 
     }
 
     @PUT
     @Path("/promo/activate/Categorie")
-    protected void activerPromoCategorie(){
+    protected void activerPromoCategorie() {
 
     }
 
     @DELETE
     @Path("/promo/desactivate/Categorie")
-    protected void desactiverPromoCategorie(){
+    protected void desactiverPromoCategorie() {
 
     }
 
     @GET
     @Path("/price/total")
-    public float getPrixTotal(int id, int quantity){
-        return 0;
+    public float getPrixTotal(int id, int quantity) {
+        return price * quantity;
     }
+
+    // pour calculer prix total d'une panier
+    @GET
+    @Path("/price/total")
+    public float getPrixTotalCart(int id, List<Cart> cartList) {
+        float total = 0;
+        for (int i = 0; i < cartList.size(); i++) {
+
+            price = cartList.getPrice();
+            total = total + price;
+
+        }
+        return total;
+    }
+
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return float return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return float return the promo
+     */
+    public float getPromo() {
+        return promo;
+    }
+
+    /**
+     * @param promo the promo to set
+     */
+    public void setPromo(float promo) {
+        this.promo = promo;
+    }
+
 }
