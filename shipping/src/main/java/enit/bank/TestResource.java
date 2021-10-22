38d4b7ff766f0344820163945d17afd716413ef8
Rahma Boughale <72996581+rahma2020817@users.noreply.github.com>
2021-10-22 12:44:01 +0100

    update

--- shipping/src/main/java/enit/bank/TestResource.java
+++ shipping/src/main/java/enit/bank/TestResource.java
@@ -5,14 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
-
-@Path("/api/persons")
+@Path("/delivery")
 public class TestResource {
 
-   
     @GET
-    @Path("/hello")
+    @Path("/delivey")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
