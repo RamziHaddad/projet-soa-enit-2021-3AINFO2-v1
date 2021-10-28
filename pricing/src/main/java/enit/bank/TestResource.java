7e14fe57bcd8cbeacc6f58321e4ff34909cb2c41
Rahma Boughale <72996581+rahma2020817@users.noreply.github.com>
2021-10-28 22:41:16 +0100

    BD and Pricing updata

--- pricing/src/main/java/enit/bank/TestResource.java
+++ pricing/src/main/java/enit/bank/TestResource.java
@@ -1,20 +1,13 @@
 package enit.bank;
 
-import java.util.List;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import enit.bank.Entity.Price;
-import enit.bank.Entity.Product;
-import enit.bank.Service.PriceService;
 
 @ApplicationScoped
 @Path("/")
